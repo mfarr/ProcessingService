08580a6dca79ff79201cd33e73c5604826c4b9aa
Mike Farr <mike.farr@tessituranetwork.com>
2022-11-30 14:58:29 -0500

    Return all schedules on GET

--- src/ProcessingService.Api/Function.cs
+++ src/ProcessingService.Api/Function.cs
@@ -1,5 +1,11 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using ProcessingService.Common.Models;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -8,20 +14,43 @@ namespace ProcessingService.Api;
 
 public class Function
 {
+    private readonly DynamoDBContext _dynamoDbContext;
+
+    public Function()
+    {
+        var dynamoDbClient = new AmazonDynamoDBClient();
+
+        _dynamoDbContext = new DynamoDBContext(dynamoDbClient);
+    }
     
     /// <summary>
-    /// A simple function that takes a string and does a ToUpper
+    /// A simple function that returns all scheduled jobs in a JSON format.
     /// </summary>
     /// <param name="request"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
+        var results = await GetAllOrganizationSchedulesAsync();
+
+        var resultsAsJson = JsonSerializer.Serialize(results);
+        
         return new APIGatewayProxyResponse
         {
-            Body = "Tessitura Processing Service",
-            Headers = new Dictionary<string, string> { {"Content-Type", "text/plain"} },
+            Body = resultsAsJson,
+            Headers = new Dictionary<string, string> { {"Content-Type", "application/json"} },
             StatusCode = 200
         };
+    }
+
+    private async Task<IEnumerable<OrganizationSchedule>> GetAllOrganizationSchedulesAsync()
+    {
+        var table = _dynamoDbContext.GetTargetTable<OrganizationSchedule>();
+
+        var search = table.Scan(new ScanOperationConfig());
+
+        var results = await search.GetNextSetAsync();
+
+        return _dynamoDbContext.FromDocuments<OrganizationSchedule>(results);
     }
 }
