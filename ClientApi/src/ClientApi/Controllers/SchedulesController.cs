230faaac0a77f8ba5ac850b454ee875469c9b872
Mike Farr <mike.farr@tessituranetwork.com>
2022-12-02 15:22:15 -0500

    Remove sample code

--- ClientApi/src/ClientApi/Controllers/SchedulesController.cs
+++ ClientApi/src/ClientApi/Controllers/SchedulesController.cs
@@ -25,31 +25,6 @@ public class SchedulesController : ControllerBase
         return await GetAllOrganizationSchedulesAsync();
     }
 
-    // GET api/values/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
-
-    // POST api/values
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
-    
     private async Task<IEnumerable<OrganizationSchedule>> GetAllOrganizationSchedulesAsync()
     {
         var table = _dynamoDbContext.GetTargetTable<OrganizationSchedule>();
